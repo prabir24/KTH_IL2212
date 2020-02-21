8ea3fd1746ecd424ec4d2bb38011de4bcb6279b2
Nakita <ozanakita@gmail.com>
2020-02-21 12:54:00 +0000

    Latest Py File

--- sobelArray.py
+++ sobelArray.py
@@ -1,10 +1,10 @@
 import re
-
-fo1 = open("outputArray2.h", "w")
+"""
+fo1 = open("flag_tiny_03_1_op.h", "w")
 fo1.write("unsigned char test_ppm_1[] = {")
 
 # Open a file
-file1 = open("image.ppm", "r+")
+file1 = open("flag_tiny_03_1.ppm", "r+")
 Lines = file1.readlines()
 count = 0
 # Strips the newline character
@@ -22,3 +22,19 @@ fo1.write("};")
 
 file1.close()
 fo1.close()
+
+"""
+with open('flag_tiny_03_1.ppm', 'r') as file:
+    data = file.read().replace('\n', ',')
+    data1 = data[0:-1].replace(' ', ',')
+    data2 = data1.replace(',,', ',')
+    data3 = data2.replace('P3,#,Created,by,IrfanView,', '')
+
+print(data)
+print(data1)
+print(data2)
+print(data3)
+
+
+with open('flag_tiny_03_1_op.h', 'w') as file:
+    file.write('unsigned char test_ppm_1[] = {' + data3 + '};')
